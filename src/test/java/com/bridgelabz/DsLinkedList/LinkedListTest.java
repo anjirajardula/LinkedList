41ea50d9f0a593613a3f634329b0e6d41aeccebb
anjirajardula <anjirajardula@gmail.com>
2021-09-12 16:53:16 +0530

    Added: create linkedlist

---
+++ src/test/java/com/bridgelabz/DsLinkedList/LinkedListTest.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.DsLinkedList;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class LinkedListTest {
+    @Test
+    public void shouldAnswerWithTrue(){
+
+            assertTrue( true );
+        }
+    }
+
